0a431e9b9a57b145119c4d7b19c738463cb7b6b4
sebPhilippot <philippot.sebastien@gmail.com>
2025-02-15 10:26:05 +0100

    initial commit

---
+++ src/main/java/com/example/TrustInsure/TrustInsureApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrustInsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustInsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustInsureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrustInsure/TrustInsureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrustInsure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustInsureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
